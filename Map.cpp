524eb1a127f486c776deedbbf0c0802dbaa86a71
SenorGato <senoraraton@gmail.com>
2021-12-02 21:34:48 +0000

    working

--- Map.cpp
+++ Map.cpp
@@ -9,11 +9,12 @@ Map::Map(int w, int h, int tWidth, int tHeight) {
 }
 
 std::vector<Tile> Map::init(){
+        std::cout <<"In init loop\n";
        int numXTiles, numYTiles;
        numXTiles = width / tileWidth;
        numYTiles = height / tileHeight;
-       for(int i; i< numXTiles; i++) {
-               for(int p; p< numYTiles; p++) {
+       for(int i = 0; i< numXTiles; i++) {
+               for(int p = 0; p< numYTiles; p++) {
                         grid.push_back (Tile());
                }
        }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h>
 #include <string>
 #include <cstdio>
+#include <iostream>
 #include "Map.h"
 
 int main (int argc, char* argv[]) {
@@ -12,6 +13,10 @@ int main (int argc, char* argv[]) {
 
     Map gamestate(800,800,10,10);
     gamestate.init();
+    
+//    std::cout << "gamestate.grid:" << gamestate.grid << std::endl;
+    std::cout << "grid.size():" << gamestate.grid.size() << std::endl;
+
 //    for(int i = 0; i < (width/tileWidth); i++) {
 //        if (i% = 0) {
 //            gamestate.sprite = "1";
